5eb0eb57bce2700dd159e5c10a5a1b2181454b57
Omar Crespo <crespomerchano@gmail.coml>
2025-03-22 20:18:01 -0500

    flake parts

--- flake.nix
+++ flake.nix
@@ -3,29 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.vimUtils.buildVimPlugin {
-            name = "mcphub.nvim";
-            src = self;
-            nvimSkipModule = [
-              "mcphub.hub"
-              "mcphub.extensions.codecompanion"
-              "mcphub"
-            ];
-          };
-        });
+  outputs = inputs @ { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { system, ... }: let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.vimUtils.buildVimPlugin {
+          name = "mcphub.nvim";
+          src = self;
+          nvimSkipModule = [
+            "mcphub.hub"
+            "mcphub.extensions.codecompanion"
+            "mcphub"
+          ];
+        };
+      };
     };
 }
 
