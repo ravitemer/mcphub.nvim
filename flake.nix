2143c6d5cff32c413d9359425a2a1596a6e88094
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2025-05-26 08:44:59 +0200

    fix: add plenary as a dependency in nix flake

--- flake.nix
+++ flake.nix
@@ -18,25 +18,13 @@
         packages.default = pkgs.vimUtils.buildVimPlugin {
           name = "mcphub.nvim";
           src = self;
+          dependencies = [ pkgs.vimPlugins.plenary-nvim ];
           nvimSkipModule = [
-            "mcphub"
-            "mcphub.hub"
             "bundled_build"
             "mcphub.extensions.avante"
             "mcphub.extensions.codecompanion"
             "mcphub.extensions.codecompanion.xml_tool"
             "mcphub.extensions.lualine"
-            "mcphub.native.neovim.lsp"
-            "mcphub.native.neovim.terminal"
-            "mcphub.native.neovim.files.search"
-            "mcphub.native.neovim.files.write"
-            "mcphub.native.neovim.files.operations"
-            "mcphub.native.neovim.files.init"
-            "mcphub.native.neovim.files.replace"
-            "mcphub.native.neovim.init"
-            "mcphub.native.neovim.prompts"
-            "mcphub.native.mcphub.init"
-            "mcphub.native.mcphub.guide"
           ];
         };
       };
