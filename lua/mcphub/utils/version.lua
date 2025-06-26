ced93307fd7ea5f4a13feba984cf8d682226958d
ravitemer <ravitemer@gmail.com>
2025-06-26 08:58:18 +0000

    chore(release): v5.11.0

--- lua/mcphub/utils/version.lua
+++ lua/mcphub/utils/version.lua
@@ -1,9 +1,9 @@
 return {
-    PLUGIN_VERSION = "5.10.0", -- Current plugin version
+    PLUGIN_VERSION = "5.11.0", -- Current plugin version
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 3,
-        minor = 6,
+        minor = 7,
         patch = 0,
-        string = "3.6.0",
+        string = "3.7.0",
     },
 }
