dd9a6949f6ddfaecb8e191bc2a08de2f3872e82d
ravitemer <ravitemer@gmail.com>
2025-07-15 16:29:42 +0000

    feat: prepare for project-aware mcphub

--- lua/mcphub/utils/version.lua
+++ lua/mcphub/utils/version.lua
@@ -2,8 +2,8 @@ return {
     PLUGIN_VERSION = "5.13.0", -- Current plugin version
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 4,
-        minor = 0,
+        minor = 1,
         patch = 0,
-        string = "4.0.0",
+        string = "4.1.0",
     },
 }
